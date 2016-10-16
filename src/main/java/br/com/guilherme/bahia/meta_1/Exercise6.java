05b4d773d12c030566163c9a06c680e601ac3bb4
Guilherme Bahia <gbvbahia01@gmail.com>
2016-10-16 20:44:14 -0200

    Exercise 6 done

---
+++ src/main/java/br/com/guilherme/bahia/meta_1/Exercise6.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.guilherme.bahia.meta_1;
+
+/**
+ * 6) Which of the following are true?
+    ( ) The Void class extends the Class class.
+    ( ) The Float class extends the Double class.
+    ( ) The System class extends the Runtime class
+    (X) The Integer class extends the Number class. TRUE
+ * @author Guilherme
+ */
+public class Exercise6 {
+}
